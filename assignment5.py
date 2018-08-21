b9822fb26d12606b94c541b0a766192bac7cdec5
arpitgarg25 <42351800+arpitgarg25@users.noreply.github.com>
2018-08-22 00:09:06 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,29 @@
+#Q=5
+a=int(input("enter quantity"))
+a=a*100
+if a>1000:
+    a=a-(a*0.1)
+print("total cost is: ",a)
+
+#Q=6
+a=[]
+for i in range(10):
+    value=int(input('Enter value: '))
+    a.append(value)
+print("List: ",a)
+
+
+#Q=8
+a=[]
+b=[]
+for i in range(4):
+    n=int(input(""))
+    a.append(n)
+    b.append(n*n)
+print(a)
+print(b)
+
+#Q=7
+a=0
+while a==0:
+    print("arpit garg")
